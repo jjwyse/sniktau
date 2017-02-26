679e65cdd61ba9b6049d1eb6d1488c63158011ec
jjwyse <josh@cloud-elements.com>
2017-02-26 13:11:05 -0700

    :information_desk_person: CI/CD may or may not work

--- bin/redeploy.sh
+++ bin/redeploy.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env sh
 
 export PATH=/opt/node/bin:$PATH
-
 cd /home/sniktau/workspace/sniktau
+
+## make sure dependencies are up-to-date
+npm i
+
+# fire 'er up'
 npm run start:deploy
