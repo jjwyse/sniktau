1deccab7ae460ceaeeb5aba1d84bea91233beaa0
jjwyse <josh@cloud-elements.com>
2017-02-27 08:32:14 -0700

    :egg: temp fix

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -8,7 +8,7 @@ ssh sniktau@$APP_SERVER 'bash -s' < bin/stop-all.sh
 
 # scp all necessary artifacts
 scp -r $BASE_DIR/dist sniktau@$APP_SERVER:/home/sniktau/workspace/sniktau
-scp -r $BASE_DIR/src/assets sniktau@$APP_SERVER:/home/sniktau/workspace/sniktau/dist/assets
+scp -r $BASE_DIR/src/assets/img sniktau@$APP_SERVER:/home/sniktau/workspace/sniktau/dist/assets
 scp -r $BASE_DIR/package.json sniktau@$APP_SERVER:/home/sniktau/workspace/sniktau
 scp -r $BASE_DIR/src/server sniktau@$APP_SERVER:/home/sniktau/workspace/sniktau/src
 scp -r $BASE_DIR/.babelrc sniktau@$APP_SERVER:/home/sniktau/workspace/sniktau
