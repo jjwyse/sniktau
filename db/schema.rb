c461eb7a88b8ecc790163054daf342ff5731d1e2
jjwyse <joshua.wyse@gmail.com>
2022-06-11 15:24:50 -0600

    Create initial DB migrations for users, activities, locations

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_20_212904) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "activities", id: :uuid, default: nil, force: :cascade do |t|
+    t.uuid "user_id", null: false
+    t.integer "strava_id", null: false
+    t.text "strava_type", null: false
+    t.text "strava_name", null: false
+    t.decimal "strava_distance", precision: 10, scale: 2, null: false
+    t.integer "strava_moving_time", null: false
+    t.integer "strava_elapsed_time", null: false
+    t.integer "strava_total_elevation_gain", null: false
+    t.datetime "strava_start_date", precision: nil
+    t.timestamptz "strava_start_date_local"
+    t.text "strava_timezone"
+    t.text "strava_map_id"
+    t.text "strava_map_summary_polyline"
+    t.integer "strava_elev_high"
+    t.integer "strava_elev_low"
+  end
+
+  create_table "locations", id: :uuid, default: nil, force: :cascade do |t|
+    t.uuid "activity_id", null: false
+    t.float "lat", null: false
+    t.float "lng", null: false
+  end
+
+  create_table "users", id: :uuid, default: nil, force: :cascade do |t|
+    t.text "bearer_token"
+    t.integer "strava_id", null: false
+    t.text "strava_username", null: false
+    t.text "strava_first_name", null: false
+    t.text "strava_last_name", null: false
+    t.text "strava_city"
+    t.text "strava_state"
+    t.text "strava_country"
+  end
+
+  add_foreign_key "activities", "users", name: "activities_user_id_fkey"
+  add_foreign_key "locations", "activities", name: "locations_activity_id_fkey"
 end
