66278fb90eff906354687484d850cabbbbbd7e01
jjwyse <joshua.wyse@gmail.com>
2022-08-02 10:40:49 -0600

    FE checkpoint

---
+++ public/static/app.mjs
@@ -0,0 +1,5 @@
+const map = L.map('map').setView([39.742043, -104.991531], 13);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '© OpenStreetMap'
+}).addTo(map);
