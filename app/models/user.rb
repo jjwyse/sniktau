b34dd5da0ad9802f9f82a84b72953296c51076cd
jjwyse <joshua.wyse@gmail.com>
2022-08-02 10:17:34 -0600

    Add has_many to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :activities
   has_many :locations, through: :activities
+  has_many :user_mountains
   validate :strava_id, :strava_username, :strava_first_name, :strava_last_name
 end
